a4719862568e43e444163d6909578de7c989134c
현청 박 <qkrguscjd2@naver.com>
2020-06-22 14:49:35 +0900

    .

--- AlgorithmHW3/AlgorithmHW3/AlgorithmHW3.cpp
+++ AlgorithmHW3/AlgorithmHW3/AlgorithmHW3.cpp
@@ -8,75 +8,95 @@ using namespace std;
 
 char route[LEN];//이동경로를 담을 배열
 char DRT[5] = { '?','U','D','R','L' }; //방향을 기록하는 배열
+int DRTN[4] = { 0,0,0,0 };
 bool visited[MAX][MAX];
 int result = 0; //최종출력, 경로 갯수
 
 //범위를 벗어났는지 판단하는 함수
-bool OutofRange(int row, int col) { 	
-	if (row > -1 && row < MAX - 1 && col > -1 && col < MAX - 1) return true;
-	return false;
+bool OutofRange(int row, int col,int cnt) { 	
+	if (row == 6 && col == 0 && cnt < LEN-2) return false;
+	
+	if ((row == 6 || row == 0) &&(col != 0) && (col != 6) ) {
+		if ((!visited[row][col - 1]) && (!visited[row][col + 1])) return false;
+	}
+	if ((col == 6 || col == 0) && (row != 0) && (row != 6)) {
+		if ((!visited[row+1][col]) && (!visited[row-1][col])) return false;
+	}
+
+	if (row < 0) return false;
+	if (row > 6) return false;
+	if (col < 0) return false;
+	if (col > 6)return false;
+	return true;
 }
 
 //d 방향으로의 이동이 가능한지 판단하는 함수
-bool isValid(char d,int row,int col) {
+bool isValid(char d,int row,int col,int cnt) {
 	switch (d)
 	{
 	case 'U':
-		if (!visited[row - 1][col] && OutofRange(row - 1, col)) return true;
-		return false;
+		if (!visited[row - 1][col]) return false;
+		if (!OutofRange(row - 1, col, cnt)) return false;
 		break;
 	case 'D':
-		if (!visited[row + 1][col] && OutofRange(row + 1, col)) return true;
-		return false;
+		if (!visited[row - 1][col]) return false;
+		if (!OutofRange(row + 1, col, cnt)) return false;
 		break;
 	case 'R':
-		if (!visited[row][col + 1] && OutofRange(row, col + 1)) return true;
-		return false;
+		if (!visited[row - 1][col]) return false;
+		if (!OutofRange(row , col + 1, cnt)) return false;
 		break;
 	case 'L':
-		if (!visited[row][col - 1] && OutofRange(row, col -1)) return true;
-		return false;
+		if (!visited[row - 1][col]) return false;
+		if (!OutofRange(row , col - 1, cnt)) return false;
 		break;
-	default:
-		return false;
 	}
+	return true;
 }
 
 
 void backtracking(int cnt, int row, int col) {
-	if (cnt == LEN-2) {
+	if (cnt == LEN - 1) {
 		result += 1;
-		cout << result<<endl;
+		printf("Result : %d!!!!!!!!!!!!!!\n\n\n\n\n\n\n", result);
 		return;
 	}
+	else if (cnt > LEN - 1) return;
 
-	for (int i = 1; i < 5;i++) {
 		if (route[cnt] != '?') {//경로가 정해져있을때
-			if (isValid(route[cnt], row, col)) {
+			if (isValid(route[cnt], row, col,cnt)) {
 				//printf("Before cnt %d)row : %2d col : %2d\n", cnt, row, col);
 				switch (route[cnt])
 				{
 				case 'U':
-					//cout << "U" << endl;
+					cout << "U" << endl;
+					cout << "row : " << row << " col :" << col << " cnt : " << cnt << endl;
 					visited[row - 1][col] = 1;
+					DRTN[0] += 1;
 					backtracking(cnt + 1, row - 1, col);
 					visited[row - 1][col] = 0;
 					break;
 				case 'D':
-					//cout << "D" << endl;
+					cout << "D" << endl;
+					cout << "row : " << row << " col :" << col << " cnt : " << cnt << endl;
 					visited[row + 1][col] = 1;
+					DRTN[1] += 1;
 					backtracking(cnt + 1, row + 1, col);
 					visited[row + 1][col] = 0;
 					break;
 				case 'R':
-					//cout << "R" << endl; 
+					cout << "R" << endl;
+					cout << "row : " << row << " col :" << col << " cnt : " << cnt << endl;
 					visited[row][col + 1] = 1;
+					DRTN[2] += 1;
 					backtracking(cnt + 1, row, col + 1);
 					visited[row][col + 1] = 0;
 					break;
 				case 'L':
-					//cout << "L" << endl;
+					cout << "L" << endl;
+					cout << "row : " << row << " col :" << col << " cnt : " << cnt << endl;
 					visited[row][col - 1] = 1;
+					DRTN[3] += 1;
 					backtracking(cnt + 1, row, col - 1);
 					visited[row][col - 1] = 0;
 					break;
@@ -87,39 +107,50 @@ void backtracking(int cnt, int row, int col) {
 				return;
 			}
 		}
-		else if (isValid(DRT[i], row, col)) {//가능한 방향
-			//printf("Before cnt %d)row : %2d col : %2d\n", cnt, row, col);
-			
-			switch (DRT[i])
-			{
-			case 'U':
-				//cout << "U" << endl;
-				visited[row - 1][col] = 1;
-				backtracking(cnt + 1, row - 1, col);
-				visited[row - 1][col] = 0;
-				break;
-			case 'D':
-				//cout << "D" << endl;
-				visited[row + 1][col] = 1;
-				backtracking(cnt + 1, row + 1, col);
-				visited[row + 1][col] = 0;
-				break;
-			case 'R':
-				//cout << "R" << endl;
-				visited[row][col + 1] = 1;
-				backtracking(cnt + 1, row , col + 1);
-				visited[row][col + 1] = 0;
-				break;
-			case 'L':
-				//cout << "L" << endl;
-				visited[row][col-1] = 1;
-				backtracking(cnt + 1, row , col -1);
-				visited[row][col - 1] = 0;
-				break;
+		else{//가능한 방향
+			for (int i = 1; i < 5;i++) {
+				if (isValid(DRT[i], row, col,cnt)) {
+					switch (DRT[i])
+					{
+					case 'U':
+						cout << "U" << endl;
+						cout << "row : " << row << " col :" << col << " cnt : " << cnt << endl;
+						visited[row - 1][col] = 1;
+						DRTN[0] += 1;
+						backtracking(cnt + 1, row - 1, col);
+						visited[row - 1][col] = 0;
+						break;
+					case 'D':
+						cout << "D" << endl;
+						cout << "row : " << row << " col :" << col << " cnt : " << cnt << endl;
+						visited[row + 1][col] = 1;
+						DRTN[1] += 1;
+						backtracking(cnt + 1, row + 1, col);
+						visited[row + 1][col] = 0;
+						break;
+					case 'R':
+						cout << "R" << endl;
+						cout << "row : " << row << " col :" << col << " cnt : " << cnt << endl;
+						visited[row][col + 1] = 1;
+						DRTN[2] += 1;
+						backtracking(cnt + 1, row, col + 1);
+						visited[row][col + 1] = 0;
+						break;
+					case 'L':
+						cout << "L" << endl;
+						cout << "row : " << row << " col :" << col << " cnt : " << cnt << endl;
+						visited[row][col - 1] = 1;
+						DRTN[3] += 1;
+						backtracking(cnt + 1, row, col - 1);
+						visited[row][col - 1] = 0;
+						break;
+					}
+				}
 			}
+			
 				
 		}
-	}
+	
 	return;
 }
 
@@ -129,6 +160,14 @@ int main()
 	//문자열 입력 받기
 	for (int i = 0;i < LEN - 2;i++) {
 		cin >> route[i];
+	}
+	if (route[0] == 'U' || route[0] == 'L') {
+		cout << 0;
+		return 0;
+	}
+	if (route[47] == 'U' || route[47] == 'R') {
+		cout << 0;
+		return 0;
 	}
 	visited[0][0] = 1;
 	backtracking(0, 0, 0);
