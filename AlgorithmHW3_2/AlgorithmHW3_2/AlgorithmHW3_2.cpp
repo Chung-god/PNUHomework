fd485b3d1de4c26f286234dffe6c42339605ce1a
HyunChungPark <qkrguscjd2@naver.com>
2020-06-23 23:25:17 +0900

    Expand

--- AlgorithmHW3_2/AlgorithmHW3_2/AlgorithmHW3_2.cpp
+++ AlgorithmHW3_2/AlgorithmHW3_2/AlgorithmHW3_2.cpp
@@ -4,54 +4,62 @@ using namespace std;
 
 int map[9][9] = {
 	{2, 2, 2, 2, 2, 2, 2, 2, 2},
-	{2, 0, 0, 0, 0, 0, 0, 0, 2},
-	{2, 0, 0, 0, 0, 0, 0, 0, 2},
-	{2, 0, 0, 0, 0, 0, 0, 0, 2},
-	{2, 0, 0, 0, 0, 0, 0, 0, 2},
-	{2, 0, 0, 0, 0, 0, 0, 0, 2},
-	{2, 0, 0, 0, 0, 0, 0, 0, 2},
-	{2, 0, 0, 0, 0, 0, 0, 0, 2},
+	{2, 0, 0, 0, 2, 0, 0, 0, 2},
+	{2, 0, 0, 0, 2, 0, 0, 0, 2},
+	{2, 0, 0, 0, 2, 2, 2, 2, 2},
+	{2, 2, 2, 2, 2, 0, 0, 2, 2},
+	{2, 0, 0, 0, 2, 2, 0, 2, 2},
+	{2, 0, 0, 0, 2, 0, 0, 0, 2},
+	{2, 0, 0, 0, 2, 0, 0, 0, 2},
 	{2, 2, 2, 2, 2, 2, 2, 2, 2}
 };
 
-void visit(int x, int y);
 
-int success;
-int start_x, start_y;
-int end_x, end_y;
+void visit(int x, int y,int cnt);
+
+int success = 0;
 
 
 int main()
 {
-	start_x = 1;
-	start_y = 1; // 시작지점
-	end_x = 7;
-	end_y = 1;  // 최종도착지점
-
-	success = 0;
-
 	printf("미로 찾기\n");
 
-	visit(1, 1);
+	visit(1, 1,0);
+	cout << success << endl;
 }
 
-void visit(int x, int y)
+void visit(int x, int y,int cnt)
 {
-	map[x][y] = 1;      // 지나간 위치를 표시
+	map[x][y] = 2;      // 지나간 위치를 표시
 	printf("(%d,%d) ", x, y);
-	printf("Find End\n");
 
-	if (x == end_x && y == end_y)   // 최종 도착지점
+	if (x == 3 && y == 1 && cnt == 8 ) {//카운터는 전체 수 - 1
+		// 최종 도착지점
 		success += 1;
-
-	if (success == 0 && map[x][y + 1] == 0)
-		visit(x, y + 1);
-	if (success == 0 && map[x + 1][y] == 0)
-		visit(x + 1, y);
-	if (success == 0 && map[x][y - 1] == 0)
-		visit(x, y - 1);
-	if (success == 0 && map[x - 1][y] == 0)
-		visit(x - 1, y);
+		cout << "찾음!!!!!!!!!!!!!!!!!!!!!!" << endl << endl;
+		map[x][y] = 0;
+		return;
+	}
+	if (map[x][y + 1] == 0)//R
+		visit(x, y + 1,cnt+1);
+	else {
+		if (map[x - 1][y + 1] == 0 && map[x + 1][y + 1] == 0) return;
+	}
+	if (map[x + 1][y] == 0)//D
+		visit(x + 1, y,cnt+1);
+	else {
+		if (map[x + 1][y - 1] == 0 && map[x + 1][y + 1] == 0) return;
+	}
+	if (map[x][y - 1] == 0)
+		visit(x, y - 1,cnt+1);
+	else {
+		if (map[x + 1][y - 1] == 0 && map[x - 1][y - 1] == 0) return;
+	}
+	if (map[x - 1][y] == 0)
+		visit(x - 1, y,cnt+1);
+	else {
+		if (map[x - 1][y - 1] == 0 && map[x - 1][y + 1] == 0) return;
+	}
 
 	map[x][y] = 0; //회귀  
 }
